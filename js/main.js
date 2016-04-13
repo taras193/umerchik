0b41d77d5a69e7e5ffd5811bb11eb62899fbb935
play0for0fun <triowork2@gmail.com>
2016-04-13 05:28:05 +0300

    animate+fullpage

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
    $('#pages').fullpage({
       afterLoad: function(anchorLink, index){
-            //$(this).find('.animation').addClass('fade-up-custom');
-            //$(this).find('.animation2').addClass('fade-up-custom2');
-            //$(this).find('.animation3').addClass('fade-up-custom3');
+            $(this).find('.animation').addClass('fade-up-custom');
+            $(this).find('.animation2').addClass('fade-up-custom2');
+            $(this).find('.animation3').addClass('fade-up-custom3');
+            $(this).find('.animation4').addClass('fade-in-custom');
           },
       onLeave: function(index, nextIndex, direction){
             $('.section:nth-child('+nextIndex+')').find('.animation').addClass('fade-up-custom');
@@ -12,12 +13,12 @@ $(document).ready(function() {
             $('.section:nth-child('+nextIndex+')').find('.animation4').addClass('fade-in-custom');
             //$('.section:nth-child('+nextIndex+')').find('.animation2').addClass('fadeInDown animated');
           },
-      afterRender: function(){
-            setTimeout(function(){$('section.section.active').find('.animation').addClass('fade-up-custom')},100);
-            setTimeout(function(){$('section.section.active').find('.animation2').addClass('fade-up-custom2')},100);
-            setTimeout(function(){$('section.section.active').find('.animation3').addClass('fade-up-custom3')},100);
-            setTimeout(function(){$('section.section.active').find('.animation4').addClass('fade-in-custom')},100);
+      //afterRender: function(){
+      //      setTimeout(function(){$('section.section.active').find('.animation').addClass('fade-up-custom')},100);
+      //      setTimeout(function(){$('section.section.active').find('.animation2').addClass('fade-up-custom2')},100);
+      //      setTimeout(function(){$('section.section.active').find('.animation3').addClass('fade-up-custom3')},100);
+      //     setTimeout(function(){$('section.section.active').find('.animation4').addClass('fade-in-custom')},100);
             
-      }
+      //}
     });
  });
